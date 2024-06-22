3b147db7ace9a6da8f5d28e8c1c98e27c404c946
HammadAli132 <alihammad132@outlook.com>
2024-06-22 20:00:23 +0500

    Added scroll behaviour on button click

--- src/js/header.js
+++ src/js/header.js
@@ -2,10 +2,18 @@ function loadHeader() {
     const header = document.createElement('header');
     const nav = document.createElement('nav');
     const btnNames = ['Home', 'Menu', 'About'];
+    const sectionIDs = ['hero', 'menu', 'about'];
+    let i = 0;
     btnNames.forEach(btn => {
         const button = document.createElement('button');
         button.innerText = btn;
+        button.setAttribute('data-scroll-to', sectionIDs[i]);
+        button.addEventListener('click', () => {
+            const section = document.getElementById(button.getAttribute('data-scroll-to'));
+            section.scrollIntoView({behavior: "smooth"});
+        });
         nav.appendChild(button);
+        i++;
     });
     const title = document.createElement('h1');
     title.innerText = 'Spicy Foods';
